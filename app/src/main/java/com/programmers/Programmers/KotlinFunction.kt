e35a7f34d9f9d9a3970ee15f7aac9a5349c50beb
kimriri <dmsal547@gmail.com>
2022-06-05 15:35:17 +0900

    신고 결과 받기/Kotlin 함수 정리

--- app/src/main/java/com/programmers/Programmers/KotlinFunction.kt
+++ app/src/main/java/com/programmers/Programmers/KotlinFunction.kt
@@ -1,18 +1,19 @@
 package com.programmers.Programmers
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class KotlinFunction : AppCompatActivity() {
+class KotlinFunction {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        GroupBy()
-    }
-
-    // GroupBy
-    private fun GroupBy() {
+   /**
+    * 출력 결과
+    * {코=[코, 코틀, 코틀린]}
+    * {코=[코], 코틀=[코], 코틀린=[코]}
+    * {코=[코], 코틀=[코틀], 코틀린=[코틀린]}
+    * {코=[코, 코틀, 코틀린]}
+    * {틀=[코, 코틀, 코틀린]}
+    * {코틀=[코, 코틀, 코틀린]}
+    * {1=[코], 2=[코틀], 3=[코틀린]}
+   */
+     fun GroupBy() {
         val GrupByList = listOf("코", "코틀", "코틀린")
         val map = GrupByList.groupBy({ "코" }, { it })
         val map1 = GrupByList.groupBy({ it }, { "코" })
@@ -31,5 +32,35 @@ class KotlinFunction : AppCompatActivity() {
         println(map6)
         println("asSequence $asSequenceFormList")
     }
+
+
+   /** .asSequence()는 데이터의 양이 많거나 용량이클때 사용하며
+    * 양이 적거나 용량이 적을 경우 오히려 연산 속도가 덜어진다 ...
+    * AsSequence0의 연산시간 9~7
+    * AsSequence1의 연산시간 1~0
+   */
+   fun AsSequence() {
+      val start0 = System.currentTimeMillis()
+
+      val AsSequence0 = arrayOf('a','b','c').asSequence()
+      println(AsSequence0.toString())
+      val end0 = System.currentTimeMillis()
+      val time0 = end0-start0
+      println("연산 시간은 $time0 입니다.")
+
+      val start1 = System.currentTimeMillis()
+      val AsSequence1 = arrayOf('a','b','c',"A","B","C","가","나","다").asSequence()
+      println(AsSequence1.toString())
+      val end1 = System.currentTimeMillis()
+      val time1 = end1-start1
+      println("연산 시간은 $time1 입니다.")
+   }
+
+   /** 출력 결과  {A=1000, B=2000, (C, 3000)=4000} */
+   fun Map() {
+      val maps = arrayOf("A" to 1000,"B" to 2000, "C" to 3000 to 4000)
+      println(maps.toMap())
+
+   }
 
 }
--- app/src/main/java/com/programmers/Programmers/MainActivity.kt
+++ app/src/main/java/com/programmers/Programmers/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.programmers.Programmers
 
+import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import androidx.annotation.RequiresApi
-import java.security.acl.Group
+import java.util.Map
 
 class MainActivity() : AppCompatActivity() {
 
-    lateinit var UserListStr: ArrayList<String>
-    lateinit var ReportStr: ArrayList<String>
+    val mKotlinFunction = KotlinFunction();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mKotlinFunction.GroupBy()
+        mKotlinFunction.AsSequence()
+        mKotlinFunction.Map()
 
-        GroupBy();
-        UserListStr =  ArrayList()
-        ReportStr = ArrayList()
+    }
+
+     init {
+        val UserListStr: ArrayList<String> =  ArrayList()
+        val ReportStr: ArrayList<String> = ArrayList()
         UserListStr.add("muzi")
         UserListStr.add("frodo")
         UserListStr.add("apeach")
@@ -31,30 +33,10 @@ class MainActivity() : AppCompatActivity() {
         ReportStr.add("muzi neo")
         ReportStr.add("apeach muzi ")
         solution(UserListStr,ReportStr,2)
-    }
-
-
-    private fun GroupBy() {
-        val GrupByList = listOf("코","코틀","코틀린")
-        val map = GrupByList.groupBy({ "코" },{ it })
-        val map1 = GrupByList.groupBy({ it },{ "코" })
-        val map2 = GrupByList.groupBy { it }
-        val map3 = GrupByList.groupBy { "코" }
-        val map4 = GrupByList.groupBy { "틀" }
-        val map5 = GrupByList.groupBy { "코틀" }
-        val map6 = GrupByList.groupBy{ it.length}
-        val asSequenceFormList = GrupByList.asSequence()
-        println(map)
-        println(map1)
-        println(map2)
-        println(map3)
-        println(map4)
-        println(map5)
-        println(map6)
-        println("asSequence $asSequenceFormList")
-
 
     }
+
+
 
         private fun solution(id_list: ArrayList<String>, report: ArrayList<String>, k: Int): IntArray =
        //report를 split 을 기준으로 쪼갠다.
@@ -67,17 +49,8 @@ class MainActivity() : AppCompatActivity() {
             .map { it[0] }
             .groupingBy { it }
             .eachCount()
-            .run { id_list.map {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                getOrDefault(it, 0)
-            } else {
-                TODO("VERSION.SDK_INT < N")
-            }
-            }.toIntArray() }
+            .run { id_list.map{ getOrDefault(it,0) }.toIntArray()}
 }
-
-
-
 
 
 
