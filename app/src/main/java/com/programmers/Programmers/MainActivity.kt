1726b3aa85e015f9c34ebc3b7799bef39f23a221
kimriri <dmsal547@gmail.com>
2022-06-12 21:30:44 +0900

    로또의 최고 순위와 최저 순위

--- app/src/main/java/com/programmers/Programmers/MainActivity.kt
+++ app/src/main/java/com/programmers/Programmers/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.programmers.Programmers
 
-import android.annotation.SuppressLint
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
-import java.util.Map
 
 class MainActivity() : AppCompatActivity() {
 
@@ -51,6 +47,63 @@ class MainActivity() : AppCompatActivity() {
             .groupingBy { it }
             .eachCount()
             .run { id_list.map{ getOrDefault(it,0) }.toIntArray()}
+}
+
+
+class Solution {
+    fun solution(lottos: IntArray, winNums: IntArray): IntArray {
+        return intArrayOf(
+            (lottos.size.plus(1)) - lottos.filter { winNums.contains(it) || it == 0 }.size,
+            (lottos.size.plus(1)) - lottos.filter(winNums::contains).size
+        ).map { if (it > 6) it - 1 else it }.toIntArray()
+    }
+}
+
+
+
+class Solutio1n {
+    fun solution(lottos: IntArray, win_nums: IntArray): MutableList<Int> {
+
+        val answer : MutableList<Int> = ArrayList()
+        val msumSize = lottos.size + win_nums.size
+
+        val joinArray : MutableList<IntArray> = ArrayList()
+        joinArray.addAll(listOf(lottos))
+        joinArray.addAll(listOf(win_nums))
+        val maxList = removeItem(joinArray,0)
+        val min = msumSize - joinArray.distinct().size
+        val max = msumSize - maxList.size
+
+        when(min) {
+            1 ->  answer.add(0,1)
+            2 ->  answer.add(0,2)
+            3 ->  answer.add(0,3)
+            4 ->  answer.add(0,4)
+            5 ->  answer.add(0,5)
+            else -> answer.add(0,6)
+        }
+
+        when(min) {
+            1 ->  answer.add(1,1)
+            2 ->  answer.add(1,2)
+            3 ->  answer.add(1,3)
+            4 ->  answer.add(1,4)
+            5 ->  answer.add(1,5)
+            else -> answer.add(1,6)
+        }
+
+
+        return answer
+    }
+
+    fun removeItem(array: MutableList<IntArray>, index: Int): Array<IntArray?> {
+        val result = arrayOfNulls<IntArray>(array.lastIndex)
+        System.arraycopy(array, 0, result, 0, index)
+        if (array.size != index) {
+            System.arraycopy(array, index + 1, result, index, array.lastIndex - index)
+        }
+        return result
+    }
 }
 
 
