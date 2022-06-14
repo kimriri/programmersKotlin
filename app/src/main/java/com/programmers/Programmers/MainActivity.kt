44d7463adeb9f3cd3a3621888189df8db86c6794
kimriri <dmsal547@gmail.com>
2022-06-15 00:43:06 +0900

    이메일 규칙 정하기 _ 아직 수정중

--- app/src/main/java/com/programmers/Programmers/MainActivity.kt
+++ app/src/main/java/com/programmers/Programmers/MainActivity.kt
@@ -2,6 +2,7 @@ package com.programmers.Programmers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.regex.Pattern
 
 class MainActivity() : AppCompatActivity() {
 
@@ -10,26 +11,30 @@ class MainActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mKotlinFunction.GroupBy()
-        mKotlinFunction.AsSequence()
-        mKotlinFunction.Map()
-        mKotlinFunction.Filter()
-        mKotlinFunction.GroupingBy()
+        val list1 = listOf(44, 1, 0, 0, 31, 25)
+        val list2 = listOf(31, 10, 45, 1, 6, 19)
+        Solution2(list1, list2)
+       // mKotlinFunction.GroupBy()
+       // mKotlinFunction.AsSequence()
+       // mKotlinFunction.Map()
+       // mKotlinFunction.Filter()
+       // mKotlinFunction.GroupingBy()
     }
 
      init {
         val UserListStr: ArrayList<String> =  ArrayList()
         val ReportStr: ArrayList<String> = ArrayList()
-        UserListStr.add("muzi")
-        UserListStr.add("frodo")
-        UserListStr.add("apeach")
-        UserListStr.add("neo")
-        ReportStr.add("muzi frodo")
-        ReportStr.add("apeach frodo")
-        ReportStr.add("frodo neo")
-        ReportStr.add("muzi neo")
-        ReportStr.add("apeach muzi ")
-        solution(UserListStr,ReportStr,2)
+//        UserListStr.add("muzi")
+//        UserListStr.add("frodo")
+//        UserListStr.add("apeach")
+//        UserListStr.add("neo")
+//        ReportStr.add("muzi frodo")
+//        ReportStr.add("apeach frodo")
+//        ReportStr.add("frodo neo")
+//        ReportStr.add("muzi neo")
+//        ReportStr.add("apeach muzi ")
+//        solution(UserListStr,ReportStr,2)
+         solution3("...Q.w/eq__%..t6...")
 
     }
 
@@ -50,16 +55,27 @@ class MainActivity() : AppCompatActivity() {
 }
 
 
-class Solution {
-    fun solution(lottos: IntArray, winNums: IntArray): IntArray {
+private fun Solution2 (lottos: List<Int>, winNums: List<Int>): IntArray{
         return intArrayOf(
             (lottos.size.plus(1)) - lottos.filter { winNums.contains(it) || it == 0 }.size,
             (lottos.size.plus(1)) - lottos.filter(winNums::contains).size
-        ).map { if (it > 6) it - 1 else it }.toIntArray()
-    }
+        ).map {
+            if (it > 6)
+                it - 1
+            else
+                it
+        }.toIntArray()
+
 }
 
-
+//1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+//2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+//3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+//4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+//5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+//6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+//만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+//7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 
 class Solutio1n {
     fun solution(lottos: IntArray, win_nums: IntArray): MutableList<Int> {
@@ -83,7 +99,7 @@ class Solutio1n {
             else -> answer.add(0,6)
         }
 
-        when(min) {
+        when(max) {
             1 ->  answer.add(1,1)
             2 ->  answer.add(1,2)
             3 ->  answer.add(1,3)
@@ -106,6 +122,29 @@ class Solutio1n {
     }
 }
 
+
+
+    fun solution3(new_id: String): String {
+        var answer = new_id.lowercase()
+        val idPattern = "^(?=.*[a-z])(?=.*[-])(?=.*[_])(?=.*[.])$"
+        val pattern = Pattern.compile(idPattern) // 패턴 컴파일
+
+        for(i in answer.indices-1) {
+            val matcher = pattern.matcher(answer[i].toString())
+            if(matcher.find()
+             //   ||(answer[i+1].toString() == "." && answer[i+2].toString() == "." )
+                ||answer.first().toString() == "."
+                ||answer.last().toString() == "."
+            ) {
+                answer = answer.replace(answer[i].toString(),"")
+            }
+
+
+
+        }
+
+        return answer
+    }
 
 
 
