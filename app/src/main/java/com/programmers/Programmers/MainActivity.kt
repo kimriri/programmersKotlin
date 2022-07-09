cd6089909f1891945051717237040c46dda25e95
김리리 <dmsal547@gmail.com>
2022-07-09 20:49:32 +0900

    Update MainActivity.kt

--- app/src/main/java/com/programmers/Programmers/MainActivity.kt
+++ app/src/main/java/com/programmers/Programmers/MainActivity.kt
@@ -216,3 +216,24 @@ class Solutio1n {
             else -> "Odd"
         }
     }
+
+    
+    
+    class Solution33 {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length-1];
+        int min = arr[0];
+        int idx = 0;
+        
+        for(int i = 1; i < arr.length; i++){
+            if(min > arr[i]) min = arr[i];
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            if(min == arr[i]) continue;
+            answer[idx++] = arr[i];
+        }
+        
+        return answer.length == 0 ? new int[]{-1} : answer;
+    }
+}
